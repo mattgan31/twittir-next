b6b484afacda71a37e3f44a80ea8a19d010911d8
mattgan31 <alamsyahr00@gmail.com>
2023-09-17 12:06:22 +0700

    Update authCheck.tsx

--- src/hooks/authCheck.tsx
+++ src/hooks/authCheck.tsx
@@ -1,5 +1,8 @@
+import axios from 'axios';
+import { getCookie } from 'cookies-next';
+
 export const checkSession = () => {
-    axios.defaults.headers.common = { 'Authorization': `Bearer ${getCookie('token')}` }
+    const token = axios.defaults.headers.common = { 'Authorization': `Bearer ${getCookie('token')}` }
     if (!token) {
         // Handle the error or redirection here
         throw new Error('User is not logged in.');
