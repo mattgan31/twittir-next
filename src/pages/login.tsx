30cea3fbff38cfee87c6e94139b3a159d03f71a6
Rahmat Alamsyah <57090921+mattgan31@users.noreply.github.com>
2023-09-25 12:35:21 +0700

    update the login & register page, update the register formik

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -32,8 +32,9 @@ export default function Login(props: any) {
 
     return (
         <div className="flex justify-center items-center h-screen">
-            <div className="flex flex-col box-content w-80 border rounded-lg p-6">
-                <h2 className="text-xl font-bold">Login</h2>
+            <div className="flex flex-col bg-slate-100 shadow-md box-content w-80 border rounded-lg p-6">
+                <h2 className="text-3xl font-bold text-center text-sky-500">Twittir</h2>
+                <h2 className="text-xl font-bold ml-2">Login</h2>
                 {loginError ? (<div className="text-center p-2 my-2 rounded-md bg-red-100"><p className="text-red-600">{loginError} </p></div>) : <></>}
                 <form onSubmit={formik.handleSubmit}>
                     <div className="p-2">
@@ -45,7 +46,7 @@ export default function Login(props: any) {
                             name="username"
                             value={formik.values.username}
                             onChange={formik.handleChange}
-                            className="rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 block w-full p-2"
+                            className="rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6 block w-full p-2"
                         />
                     </div>
                     <div className="p-2">
@@ -57,18 +58,18 @@ export default function Login(props: any) {
                             name="password"
                             value={formik.values.password}
                             onChange={formik.handleChange}
-                            className="rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 block w-full p-2"
+                            className="rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6 block w-full p-2"
                         />
                     </div>
                     <div className="p-2">
                         <button
                             type="submit"
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            className="flex w-full justify-center rounded-md bg-sky-600 px-3 py-1.5 text-base font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
                         >
                             Login
                         </button>
                         <Link href={'/register'}>
-                            <button className="flex w-full justify-center text-indigo-600 px-3 py-1.5 my-2 rounded-md bg-indigo-100 hover:bg-indigo-300 hover:text-white">Register</button>
+                            <button className="flex w-full justify-center text-sky-600 px-3 py-1.5 my-2 rounded-md font-semibold bg-slate-100 outline-sky-500 outline outline-1 outline-offset-0 font-medium hover:bg-slate-300 hover:text-sky-900">Register</button>
                         </Link>
                     </div>
                 </form>
